bb6bf45d9a2ab4d0de70cf44f8ea8c414c89698a
Leandro Ribeiro <leandro_ribeiro_23@hotmail.com>
2019-03-16 21:47:27 +0000

    Projecto funcional

--- app/src/main/java/com/example/projecto_android_leandroribeiro_final/fragment/DetailFragment.java
+++ app/src/main/java/com/example/projecto_android_leandroribeiro_final/fragment/DetailFragment.java
@@ -1,6 +1,7 @@
 package com.example.projecto_android_leandroribeiro_final.fragment;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
